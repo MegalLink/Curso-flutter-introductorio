3ed52a980a774d3293599343cc7919f96d5c5a6a
MegalLink <alexresidentevil@gmail.com>
2021-05-13 16:19:32 -0500

    StatelessWidget and some basic Windgets

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final TextStyle estiloTexto = new TextStyle(
+      fontSize: 30.0,
+      fontStyle: FontStyle.italic,
+      color: Color.fromRGBO(66, 53, 2, 0.5));
+  final contador = 10;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Titulo"),
+        centerTitle: true,
+        elevation: 10.0,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Numero de veces presionado"),
+            Text(
+              '$contador',
+              style: estiloTexto,
+            )
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {print("pressed")},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
