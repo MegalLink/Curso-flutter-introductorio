00bb63d7149f29c29cd6197a60111e0e176ffb70
MegalLink <alexresidentevil@gmail.com>
2021-05-15 12:21:00 -0500

    Alert Dialog

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,7 +7,46 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alertas'),
       ),
-     
+      body: Center(
+        child: ElevatedButton(
+          style: ButtonStyle(
+              shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50.0)))),
+          child: Text('Mostrar  Alerta'),
+          onPressed: () {
+            _mostrarAlerta(context);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.home),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Alert Dialog'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [Text('Flutter Dialog'), FlutterLogo()],
+          ),
+          actions: [
+            TextButton(
+                onPressed: () => Navigator.pop(context), child: Text('Cancel')),
+            TextButton(onPressed: () {}, child: Text('Ok')),
+          ],
+        );
+      },
     );
   }
 }
