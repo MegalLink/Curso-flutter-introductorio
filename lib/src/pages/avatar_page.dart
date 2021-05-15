f148ddb9137f1a5cb1843463910fd5ee4c718bb1
MegalLink <alexresidentevil@gmail.com>
2021-05-15 12:46:57 -0500

    Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,8 +6,28 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatars'),
+        actions: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://images.pexels.com/photos/2804282/pexels-photo-2804282.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+                scale: 0.1),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0, left: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.redAccent,
+            ),
+          ),
+        ],
       ),
-    
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/images/default-image.png'),
+          image: NetworkImage(
+              'https://images.pexels.com/photos/3460478/pexels-photo-3460478.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'),
+        ),
+      ),
     );
   }
 }
